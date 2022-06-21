1f31c03ff29087dae1e02208e95d6c5151631b36
sunwanchung <sunwanchung@gmail.com>
2022-06-21 14:36:47 +0900

    스크립트 추가중

---
+++ index.js
@@ -0,0 +1,17 @@
+const home = document.querySelector('.home')
+const yess = document.querySelector('.yess')
+const nono = document.querySelector('.nono')
+
+
+home.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-blacknoodle/'
+})
+
+nono.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-blacknoodle/'
+})
+
+
+yess.addEventListener('click',function(){
+  alert("정상적으로 전송 하였습니다")
+})
